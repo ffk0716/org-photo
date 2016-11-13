83de0d4d28b947e6360fc52171051b6eff101d1a
Eric Yu <ffk0716@gmail.com>
2016-11-13 18:33:06 +0800

    add: support hour offset

--- org-photo.py
+++ org-photo.py
@@ -3,25 +3,35 @@ import datetime
 import shutil
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-p', '--path', required=True, help='path for photos')
 parser.add_argument('-d', '--dry', action='store_true', help='dry run mode')
+parser.add_argument('-s', '--sep', type = int, default = 0, help='separate day by X:00:00')
 args = parser.parse_args()
 
 dir1 = args.path
 files = [name for name in os.listdir(dir1) if not os.path.isdir(os.path.join(dir1, name)) ]
 
+sum_day_shift = 0
 for file in files:
     try:
-        a = datetime.datetime.strptime(file[:10], '%Y-%m-%d')
+        raw_date = datetime.datetime.strptime(file[:19], '%Y-%m-%d %H.%M.%S')
     except ValueError:
         print 'ignore', file
         continue
-    dir2 = os.path.join(dir1, str(a.date()))
+    date = raw_date - datetime.timedelta(hours = args.sep)
+    post_fix = ''
+    if raw_date.date() != date.date():
+        post_fix = '*'
+        sum_day_shift += 1
+    dir2 = os.path.join(dir1, str(date.date()))
     if not os.path.exists(dir2):
         os.makedirs(dir2)
     name1 = os.path.join(dir1, file)
     name2 = os.path.join(dir2, file)
-    print name1, '->', name2
+    print name1, '->', name2, post_fix
     if not args.dry:
         shutil.move(name1, name2)
+
+if sum_day_shift:
+    print 'day-shifted files:', sum_day_shift
